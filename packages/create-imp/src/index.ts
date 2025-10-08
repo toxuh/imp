a7f3937fc11f94ef17d028bc5666c940bc9bdcb4
toxuh <zakharov.toxa@gmail.com>
2025-10-08 16:19:39 +0200

    Enhance Prisma setup prompt and add final installation message

--- packages/create-imp/src/index.ts
+++ packages/create-imp/src/index.ts
@@ -4,14 +4,15 @@ import prompts from 'prompts';
 
 export const run = async (options?: Partial<Options>): Promise<void> => {
   let withPrisma = options?.withPrisma;
+
   if (withPrisma === undefined) {
     const res = await prompts({
       type: 'toggle',
       name: 'withPrisma',
-      message: 'with prisma?',
-      initial: false,
-      active: 'yes',
-      inactive: 'no',
+      message: 'Do you want to set up Prisma ORM?',
+      initial: true,
+      active: 'Yes',
+      inactive: 'No',
     });
     withPrisma = res.withPrisma;
   }
@@ -22,4 +23,7 @@ export const run = async (options?: Partial<Options>): Promise<void> => {
     debug: options?.debug ?? false,
   };
   await runSteps(opts);
+
+  console.clear();
+  console.log('IMP was installed. Enjoy.');
 };
